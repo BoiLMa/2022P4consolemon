a670bbcf09aa4ccc7417571d0d87363954772161
unknown <30570@ma-web.nl>
2022-06-03 11:38:11 +0200

    consolemon

---
+++ ConsoleMon/ConsoleMon.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    internal class ConsoleMon
+    {
+        int health;
+        int energy;
+        string name;
+        List<Skill> skills = new List<Skill>();
+        Elements weakness;
+        
+        internal ConsoleMon()
+        {
+
+        }                           
+        internal ConsoleMon(ConsoleMon CopyFrom)//argument is tussen de haakjes
+        {
+            this.health = CopyFrom.health;
+            this.energy = CopyFrom.energy;
+            this.name = CopyFrom.name;  
+            this.weakness = CopyFrom.weakness;
+
+            for (int i = 0; i < CopyFrom.skills.Count; i++)
+            {
+                this.skills.Add(new Skill(CopyFrom.skills[i]));
+            }
+        }
+
+        public void TakeDamage(int damage)
+        {
+            health -= damage;
+        }
+        public void DepleteEnergy(int amount)
+        {
+            energy -= amount;
+        }
+    }
+}
---
+++ ConsoleMon/Skill.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    internal class Skill
+    {
+        int damage;
+        int energyCost;
+        string name;
+        Elements element;
+
+        internal Skill()
+        {
+
+        }
+        internal Skill(Skill eenskill)
+        {
+            this.damage = eenskill.damage; 
+            this.energyCost = eenskill.energyCost;
+            this.name = eenskill.name;
+            this.element = eenskill.element;
+        }
+        public void UseOn(ConsoleMon caster, ConsoleMon target)
+        {
+            caster.DepleteEnergy(energyCost);
+            target.TakeDamage(damage);
+        }
+    }
+    enum Elements
+    {
+        Fire,
+        Grass,
+        Electricity,
+        Stone,
+        Balls,
+        H2O
+    }
+}
